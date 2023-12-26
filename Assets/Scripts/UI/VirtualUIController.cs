99f6d5b37ec9b9b000156d824089b5a2ae5a6d78
dakotairsik <dakotairsik@gmail.com>
2023-12-26 16:34:44 -0500

    more work.

--- Assets/Scripts/UI/VirtualUIController.cs
+++ Assets/Scripts/UI/VirtualUIController.cs
@@ -5,10 +5,14 @@ public class VirtualUIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        Debug.Log(Application.platform);
         this.gameObject.SetActive(false);
+        this.enabled = false;
 
 #if UNITY_ANDROID || UNITY_IOS || UNITY_WEBGL
         this.gameObject.SetActive(true);
+        this.enabled = true;
 #endif
     }
 
